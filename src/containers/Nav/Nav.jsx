f0050853c4f0f9502b8f83b45e648a3a4f540386
nologyRob <rob.hooper@nology.io>
2022-09-13 16:24:51 +0100

    removed search component

--- src/containers/Nav/Nav.jsx
+++ src/containers/Nav/Nav.jsx
@@ -1,12 +1,11 @@
 import React from 'react'
-import SearchBar from '../../components/SearchBar/SearchBar'
 import "./Nav.scss"
 import logo from "../../assets/images/logo.png"
 
 const Nav = () => {
   return (
     <div className='nav'>
-      <SearchBar />
+      
       <img className='nav__logo' src={logo} alt="GOT-logo" />
     </div>
   )
